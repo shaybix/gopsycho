260c2400554363190678ce13cd9b792f062a5fc2
shaybix <shaybix@tuta.io>
2016-02-22 17:06:28 +0000

    added other methods for the other actions, although functionality isnt

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"syscall"
 )
 
 // Commandline flags/arguments specified
@@ -20,6 +19,8 @@ var (
 )
 
 // SafeFileHandle of type []byte
+// might not be neccessarily as it was a type in Windows for safe handling of files
+// however keep it for now for ease of understanding code
 type SafeFileHandle []byte
 
 // PhisonDevice of type struct
@@ -50,11 +51,14 @@ func main() {
 	//if err != nil {
 	//	log.Println("Could not dump the firmware")
 	//}
-	fmt.Println(&handle)
+	fmt.Println(handle)
 
 }
 
 // OpenDrive opens a connection with the device
+// Look into whether there is a better way of doing this
+// is it necessarilly to call another Open() method within this function?
+// However in order to stick to the way done in the original Psychson code keep it as it is.
 func OpenDrive(drive string) (*os.File, error) {
 	defer CloseDrive()
 
@@ -99,6 +103,43 @@ func CloseDrive() {
 //	return nil
 //}
 
+// Open is responsible for opening a connection
+func (d *PhisonDevice) Open() (*os.File, error) {
+
+	// TODO: open a connection
+	// However getting a read-only filesystem error which I need to look into
+
+	handle, err := os.OpenFile(d.DriveLetter, os.O_RDONLY, os.ModeDevice)
+	if err != nil {
+		log.Println("Could not open file to the device: ")
+		return nil, err
+
+	}
+
+	return handle, nil
+
+}
+
+// JumpToBootMode sets the device to boot mode
+// for when user sets --action=SetBootMode
+func (d *PhisonDevice) JumpToBootMode() error {
+
+	// TODO: SendCommand data to device to set it to boot mode
+	return nil
+}
+
+func (d *PhisonDevice) ExecuteImage() error {
+
+	// TODO: Execute the burner image
+	return nil
+}
+
+func (d *PhisonDevice) JumpToPRAM() error {
+
+	// TODO:
+	return nil
+}
+
 // SendCommand sends data across to the device
 func (d *PhisonDevice) SendCommand(handle SafeFileHandle, cmd []byte, data []byte, bytesExpected int) []byte {
 
@@ -106,29 +147,4 @@ func (d *PhisonDevice) SendCommand(handle SafeFileHandle, cmd []byte, data []byt
 
 	// TODO: Implement the SendCommand method
 	return ret
-}
-
-// Open is responsible for opening a connection
-func (d *PhisonDevice) Open() (*os.File, error) {
-
-	// TODO: open a connection
-	// However getting a read-only filesystem error which I need to look into
-
-	handle, err := os.OpenFile(d.DriveLetter, syscall.O_RDWR, os.ModeDevice)
-	if err != nil {
-		log.Println("Could not open file to the device: ")
-		return nil, err
-
-	}
-
-	fmt.Println(handle)
-	return handle, nil
-
-}
-
-// JumpToBootMode sets the device to boot mode
-func (d *PhisonDevice) JumpToBootMode() error {
-
-	// TODO: SendCommand data to device to set it to boot mode
-	return nil
 }
